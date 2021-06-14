1f8cd46caec8505ff99af55c1109a3be1fa869c1
v.iurinskaia <v.iurinskaia@bars-open.ru>
2021-06-14 16:37:32 +0300

    Added bot send message functionality

--- telegram/telegram_init.go
+++ telegram/telegram_init.go
@@ -1,7 +1,11 @@
 package telegram
 
 import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
 	"gopkg.in/telegram-bot-api.v4"
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
 )
@@ -27,4 +31,33 @@ func InitTelegram() (*tgbotapi.BotAPI, error) {
 	}
 
 	return bot, err
+}
+
+func WebhookHandler(c *gin.Context) {
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}(c.Request.Body)
+
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	var update tgbotapi.Update
+	err = json.Unmarshal(bytes, &update)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	msg := UpdateHandler(&update)
+	_, err = bot.Send(msg)
+	if err != nil {
+		log.Println(err)
+	}
+
 }
--- telegram/update_handler.go
+++ telegram/update_handler.go
@@ -1,10 +1,43 @@
 package telegram
 
 import (
+	"github.com/Valeviy/fiziklbot/telegram/commands"
+	"github.com/Valeviy/fiziklbot/telegram/messages"
 	"gopkg.in/telegram-bot-api.v4"
 	"log"
+	"reflect"
 )
 
-func UpdateHandler(u *tgbotapi.Update) {
-	log.Printf("From: %+v Text: %+v\n", u.Message.From, u.Message.Text)
+func UpdateHandler(u *tgbotapi.Update) tgbotapi.MessageConfig {
+	var msg = tgbotapi.NewMessage(u.Message.Chat.ID, "")
+
+	if u.Message != nil {
+		if u.Message.IsCommand() {
+			CommandHandler(u, &msg)
+		} else {
+			TextHandler(u, &msg)
+		}
+	} else {
+		msg.Text = messages.EMPTY
+	}
+
+	return msg
+}
+
+func CommandHandler(u *tgbotapi.Update, msg *tgbotapi.MessageConfig) {
+	switch u.Message.Command() {
+	case commands.START:
+		msg.Text = messages.START
+	default:
+		msg.Text = messages.EMPTY
+	}
+}
+
+func TextHandler(u *tgbotapi.Update, msg *tgbotapi.MessageConfig) {
+	if reflect.TypeOf(u.Message.Text).Kind() == reflect.String && u.Message.Text != "" {
+		msg.Text = u.Message.Text
+		log.Printf("From: %+v Text: %+v\n", u.Message.From, u.Message.Text)
+	} else {
+		msg.Text = messages.EMPTY
+	}
 }
